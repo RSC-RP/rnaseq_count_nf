34867b0c486ef167042345b8e852a3b7218d9cb2
jsmi26 <jenny.smith@seattlechildrens.org>
2022-11-15 16:53:29 -0800

    updated sample sheet paths, removed docker profile, clarified docs, default queue paidq

--- main_run.sh
+++ main_run.sh
@@ -3,21 +3,21 @@
 set -eu
 DATE=$(date +%F)
 NFX_CONFIG=./nextflow.config
-#Options: local_singularity, PBS_singularity, PBS_conda, and local_docker
+#Options: local_singularity, PBS_singularity, and PBS_conda
 NFX_PROFILE='PBS_singularity'
 #Options: star_index or rnaseq_count or sra_fastqs
 NFX_ENTRY='rnaseq_count'
 #The output prefix on filenames for reports/logs
 REPORT=${1:-"pipeline_report"}
 
-# Load the modules 
+# Load the modules
 # TO DO: make the module version a variable that the user can change eg SINGULARITY="singularity/3.9.9"; module load $SINGULARITY
 if [[ $NFX_PROFILE =~ "singularity" ]]
 then
     module load singularity
 fi
 
-# Nextflow run to execute the workflow 
+# Nextflow run to execute the workflow
 # TO DO: --singularity_module $SINGULARITY #the in nextflow.config could access this as params.SINGULARITY
 PREFIX="${REPORT}_${DATE}"
 nextflow -c ${NFX_CONFIG} \
